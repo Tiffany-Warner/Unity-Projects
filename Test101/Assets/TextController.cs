0866891061aeedf322c01635a34f951fa01f592c
Tiffany Warner <tiffy.warner@gmail.com>
2018-02-28 06:00:57 -0700

    Text101: Setup Files for Text101

---
+++ Test101/Assets/TextController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI; //To access Text object
+using System.Collections;
+
+public class TextController : MonoBehaviour {
+	
+	//Provides access to the Text object
+	public Text text;
+	
+	// Use this for initialization
+	void Start () {
+		text.text = "Hello World";
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
